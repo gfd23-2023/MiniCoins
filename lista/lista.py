becf63e625286dc4d0e4056a837db38939d05512
Giovanna Fioravante Dalledone <gfd23@inf.ufpr.br>
2025-11-10 18:28:59 -0300

    Limpado

--- lista/lista.py
+++
@@ -1,7 +0,0 @@
-""" 
-Implementação da lista ligada
-OBS: De acordo com o que eu pesquisei, o arquivo __init__.py serve para que o diretório seja
-reconhecido como pacote pacote importável. Dentro dele é possível controlar o que é exportado,
-agrupar, executar código de configuração, etc.
-"""
-
